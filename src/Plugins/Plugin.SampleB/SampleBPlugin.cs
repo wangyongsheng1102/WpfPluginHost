15904c66734b4869c30ab06146256359c282a9e4
wangyongsheng1102 <wangyongsheng891102@gmail.com>
2026-03-30 10:57:07 +0800

    Add files via upload

--- src/Plugins/Plugin.SampleB/SampleBPlugin.cs
+++ src/Plugins/Plugin.SampleB/SampleBPlugin.cs
@@ -7,6 +7,7 @@ public sealed class SampleBPlugin : IPluginModule
 {
     public string Id => "sampleB";
     public string Title => "Sample B";
+    public string Description => "Preview visual gallery components";
     public string IconKey => "🧩";
     public int Order => 20;
 
