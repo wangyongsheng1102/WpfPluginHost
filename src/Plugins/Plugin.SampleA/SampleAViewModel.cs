c53e7a0fb56109813cd7e9ebe5bf38865e06403b
wangyongsheng1102 <wangyongsheng891102@gmail.com>
2026-03-30 13:36:54 +0800

    Add files via upload

--- src/Plugins/Plugin.SampleA/SampleAViewModel.cs
+++ src/Plugins/Plugin.SampleA/SampleAViewModel.cs
@@ -1,6 +1,7 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Win32;
+using Plugin.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
