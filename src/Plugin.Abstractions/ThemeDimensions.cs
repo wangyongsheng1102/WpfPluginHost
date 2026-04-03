71ca6475e674465d2a17ec7d34c1194e8c17b22f
wangyongsheng1102 <wangyongsheng891102@gmail.com>
2026-04-03 08:57:46 +0800

    update

--- src/Plugin.Abstractions/ThemeDimensions.cs
+++ src/Plugin.Abstractions/ThemeDimensions.cs
@@ -1,15 +1,21 @@
 namespace Plugin.Abstractions;
 
 /// <summary>
-/// ホスト・プラグイン共通のテーマ数値。XAML の <c>clr-namespace:System;assembly=*</c> や
-/// ホスト専用の <c>StaticResource BodyFontSize</c> に依存せず、<c>x:Static</c> で参照する。
+/// ホスト・プラグイン共通のタイポグラフィとテーマ数値。<c>x:Static</c> で参照する。
 /// </summary>
 public static class ThemeDimensions
 {
     public const double TitleFontSize = 28;
+    public const double TitleCompactFontSize = 26;
     public const double SubtitleFontSize = 20;
     public const double BodyFontSize = 16;
     public const double CaptionFontSize = 15;
+    public const double SmallFontSize = 13;
+    public const double MicroFontSize = 12;
+    public const double ButtonFontSize = 16;
+    public const double DataGridHeaderFontSize = 15;
+    public const double NavIconGlyphFontSize = 24;
+    public const double CardIconGlyphFontSize = 32;
     public const double ShutdownWireDashLength = 96;
     public const double IconHoverScale = 1.08;
 }
