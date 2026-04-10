7d4d39cb108798f7d41166f1a51a2b2d4f511412
wangyongsheng1102 <wangyongsheng891102@gmail.com>
2026-04-10 08:43:35 +0800

    Update CompareOptions.cs

--- src/Plugins/Plugin.PixelCompare/Models/CompareOptions.cs
+++ src/Plugins/Plugin.PixelCompare/Models/CompareOptions.cs
@@ -2,7 +2,7 @@ namespace Plugin.PixelCompare.Models;
 
 public sealed class CompareOptions
 {
-    public int DiffThreshold { get; set; } = 30;
+    public int DiffThreshold { get; set; } = 35;
     public int MinRegionArea { get; set; } = 50;
     public int MergeDistance { get; set; } = 10;
     public int ExpandPixels { get; set; } = 3;
